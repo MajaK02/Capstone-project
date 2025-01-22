a9e6951d0389d00d916940edaecee2632d798d1c
Maja Kazmierczak <upgbhvet@students.codeinstitute.net>
2025-01-22 13:27:10 +0000

    Add about app & set up a foundation for the about page

--- config/settings.py
+++ config/settings.py
@@ -29,10 +29,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
-                 '.herokuapp.com']
+                 '.herokuapp.com',
+                 '127.0.0.1']
 
 # Application definition
 
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_summernote',
     'blog',
+    'about', 
 ]
 
 MIDDLEWARE = [
