12feea00abcb2adb26828de2277df6b70796fd18
Maja Kazmierczak <upgbhvet@students.codeinstitute.net>
2025-01-23 15:49:58 +0000

    Create initial styling with logo, navbar, and carousel

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['localhost',
                  '.herokuapp.com',
                  '127.0.0.1']
@@ -149,8 +149,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'), ]
+
 
 
 # Default primary key field type
